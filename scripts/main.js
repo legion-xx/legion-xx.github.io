3f7df926eb3f6b282984e769df28a61066fc8fca
legion-xx <100481111+legion-xx@users.noreply.github.com>
2022-03-16 07:41:39 -0700

    fafafa

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 /*
 SCRIPTS
 */
+
+
+var sku_dealer_pages = {
+    "drc-oc": '<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSMFGgAOVpuPYaQhgUCwL1auo4WGRQ6now7Kc5UTyfs6g_2wCfE2D71Iy-GgYy5B8ONJcpZs5JbT6dU/pubhtml?gid=261086542&amp;single=true&amp;widget=true&amp;headers=false"></iframe>'
+};
+
+
+function dealer_orders (company) {
+    return sku_dealer_pages[company];
+}
+
+$('main').html(dealer_orders("drc-oc"));
